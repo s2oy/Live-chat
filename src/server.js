8b45fd0c4f02b3c715e9515210140343e87e0307
백서연 <w2032@e-mirim.hs.kr>
2021-12-14 23:09:19 +0900

    Add connect app to server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 import express from "express";
 
-const app = express();
-console.log("hello");
 
-app.listen(3000);
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use('/public', express.static(__dirname + '/public')); //user가 /public으로가면 폴더를 보여줌
+app.get('/', (req, res) => res.render('home'));	//home으로 가면 req,res를 받고 home을 렌더링함
+
+const handleListen = () => console.log('Listening on http://localhost:3000');
+app.listen(3000, handleListen);
